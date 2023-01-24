958dcafd751ec89f5ad261e5c6b44aae50de53c2
JonG <teflonjuan@icloud.com>
2023-01-24 01:06:50 -0600

    made some HTML changes andadded a little bit off css and barely started java

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,7 @@
+let apiKey = "3bfd7abbb87e64e6f756c29dcd4cdfbf";
+let currentCityDisplayed = "";
+let previousCities = "";
 
+$("#clear-storage").on("click", (event) => {
+    localStorage.clear();
+});
