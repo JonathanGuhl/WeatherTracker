2b099e1520a42237399edaa001dd365974a6369a
JonG <teflonjuan@icloud.com>
2023-01-25 23:03:40 -0600

    added code to JS to fetch current weather and five day forecast

--- Assets/script.js
+++ Assets/script.js
@@ -2,8 +2,8 @@ let owmAPI = "3bfd7abbb87e64e6f756c29dcd4cdfbf";
 var currentCity = "";
 var lastCity = "";
 
+// Obtains city name from the search box
 var getCurrentConditions = (event) => {
-    // Obtain city name from the search box
     let city = $('#searchCity').val();
     currentCity= $('#searchCity').val();
     let dataSearch = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&APPID=" + owmAPI;
@@ -13,6 +13,14 @@ var getCurrentConditions = (event) => {
     })
     .then(data => {
         console.log(data);
+        let getFiveDayForecast = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + owmAPI;
+        fetch(getFiveDayForecast)
+        .then((response) => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+        })
     })
     .catch(error => {
         console.error(error);
@@ -21,17 +29,17 @@ var getCurrentConditions = (event) => {
         saveSearch(city);
     })
 }
-
+// Adds searched citites to local storage
 var saveSearch = (newCity) => {
     let cityExists = false;
-    // Check if City exists in local storage
+// Check if City exists in local storage
     for (let i = 0; i < localStorage.length; i++) {
         if (localStorage["cities" + i] === newCity) {
             cityExists = true;
             break;
         }
     }
-    // Save to localStorage if city is new
+// Save to localStorage if city is new
     if (cityExists === false) {
         localStorage.setItem('cities' + localStorage.length, newCity);
     }
@@ -44,7 +52,8 @@ event.preventDefault();
 currentCity = $('#searchCity').val();
 getCurrentConditions(event);
 });
-
+// Event listener for clear storage button
 $('#clearStorage').on("click", (event) => {
     localStorage.clear();
 });
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # WeatherTracker
+- This is incomplete at the moment
+- So far though, I have been able to pull data from the weather API and I am able to return a five day forecast and I am also able to receive todays forecast 
+- I still need to populate this data on the page 
